96c974e546e97590af8133a3362572972798d57d
AjxLab <abe12@mccc.jp>
2020-01-18 04:49:48 +0900

    add LISENCE

--- rwave/__init__.py
+++ rwave/__init__.py
@@ -1,7 +1,7 @@
 from .rwave import *
 
 __copyright__    = 'Copyright (C) 2020 Tatsuya Abe'
-__license__      = 'BSD-3-Clause'
+__license__      = 'MIT'
 __author__       = 'Tatsuya Abe'
 __author_email__ = 'abe12@mccc.jp'
 __url__          = 'https://github.com/AjxLab/PyCrawl'
